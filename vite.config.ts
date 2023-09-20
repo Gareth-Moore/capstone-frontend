34e358f229c551f62b4a0bfa7cbaba4d0de030ec
Gareth-Moore <gccsmoore3@gmail.com>
2023-09-20 13:52:21 +0100

    updated proxy

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://back-for-seconds-backend.onrender.com/",
         changeOrigin: true,
       },
     },
